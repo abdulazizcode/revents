2e159c31a51801f4c5578cd33ed2c2676fbe4d22
abdulazizcode <tajikcoder@gmail.com>
2021-04-13 15:54:13 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const rootEl = document.getElementById('root');
+
+let render = () => {
+	ReactDOM.render(<App/>, rootEl);
+}
+
+if(module.hot){
+	module.hot.accept('./app/layout/App', () =>{
+		setTimeout(render);
+	});
+};
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
